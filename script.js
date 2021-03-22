b844ae4ebbbde596db800ad02280e597bdc1ffc3
martialmania <martialmania19@gmail.com>
2021-03-22 11:15:49 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-console.log("mania");
-var details=document.getElementsByClassName("contactInformation");
-var contlabel=document.getElementsByClassName("contacts");
-contlabel.style.backgroundColor="#0000080";
-contlabel.addEventListener(onclick, add);
+console.log("Mania");
 
-function add(){
-    console.log("I am okay");
+window.oload=function what(){
+  
+
+};
+const but2=document.getElementByClass("but2");
+but2.addEventListener('click',relax);
+function relax(){
+    alert("Click on myContact");
 }
 
-var but2=document.getElementsByClassName("but_2");
-but2.addEventListener('click', num);
-
-function num(){
-    alert("+254740743521\n Email: martialmania19@gmail.com")
-}
+//var contactdetails = document.getElementById("contactInformation");
+//contactdetails.innerText="+254740743521";
